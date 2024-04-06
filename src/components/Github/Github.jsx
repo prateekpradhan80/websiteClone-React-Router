676c0d967a2267c0420c6f53fd4e2556150359e1
Prateek <prateekpradhan80@gmail.com>
2024-04-07 02:49:05 +0530

    fetching data from my github api

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,8 +1,7 @@
-
 import { useLoaderData } from "react-router-dom";
 //import React, { useState, useEffect } from "react";
 function Github() {
-  const data=useLoaderData();
+  const data = useLoaderData();
   /* you can fetch this way but another way using loader gives more optimization
   const [data, setData] = useState([]);
   useEffect(() => {
@@ -10,12 +9,17 @@ function Github() {
     .then((res) => res.json())
     .then(data=>{console.log(data); setData(data)})
   }, []);*/
-  return(<div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl" >Github Location :{data.location}
-  <img src= {data.avatar_url}alt="" /></div>) ;
+  return (
+    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
+      <h1>Name : {data.name}</h1>
+      <h2> Github Location :{data.location}</h2>
+      <img src={data.avatar_url} alt="" />
+    </div>
+  );
 }
 
 export default Github;
-export const githubInfoLoader = async ()=>{
-  const response =await fetch( `https://api.github.com/users/india`)
-  return response.json()
-}
+export const githubInfoLoader = async () => {
+  const response = await fetch(`https://api.github.com/users/prateekpradhan80`);
+  return response.json();
+};
